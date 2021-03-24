9f8ed03ca29cd86afe08a251514c739f694c725f
Michael Knowlton <mknowlton89@gmail.com>
2021-03-24 16:56:14 -0400

    Tightened up the past, present, future logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let currentDayEl = $('#currentDay');
 let currentHour = moment().format("H")
 let plannerEl = $('#planner');
 
-console.log(currentHour);
+console.log("Current hour is: " + currentHour);
 // console.log(moment(currentHour, "H").format("h"));
 
 
@@ -11,11 +11,7 @@ console.log(currentHour);
 const workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Function Definitions
-function init() {
-    // Displays the current date in the header.
-    currentDayEl.text(moment().format("dddd, MMMM Do"));
-
-
+function displayCalendar() {
     // For loop to create the blocks
     for (let i = 0; i < workHours.length; i++) {
 
@@ -24,10 +20,8 @@ function init() {
         let hourEl = $('<p>');
         let descriptionEl = $('<textarea>')
         let saveBtnEl = $('<button>');
-        let formattedHour = moment(workHours[i], "H").format("H");
 
-        console.log("Formatted hour is: " + formattedHour);
-        console.log("workHours is: " + workHours[i]);
+        // console.log("workHours is: " + workHours[i]);
 
         // I need to add classes to the elements
         rowEl.addClass('row time-block');
@@ -47,41 +41,21 @@ function init() {
         rowEl.append(descriptionEl);
         rowEl.append(saveBtnEl);
 
-        if (currentHour === formattedHour) {
+        if (currentHour == workHours[i]) {
             descriptionEl.addClass('present');
-        } else if (currentHour < formattedHour) {
+        } else if (currentHour < workHours[i]) {
             descriptionEl.addClass('future');
-        } else if (currentHour > formattedHour) {
+        } else {
             descriptionEl.addClass('past');
         }
     }
+}
 
+function init() {
+    // Displays the current date in the header.
+    currentDayEl.text(moment().format("dddd, MMMM Do"));
 
-
-    // // I need to create elements
-    // let rowEl = $('<div>');
-    // let hourEl = $('<p>');
-    // let descriptionEl = $('<textarea>')
-    // let saveBtnEl = $('<button>');
-
-    // // I need to add classes to the elements
-    // rowEl.addClass('row time-block');
-    // hourEl.addClass('hour text-right col-1');
-    // descriptionEl.addClass('past description w-100 p-3 col-10');
-    // saveBtnEl.addClass('saveBtn col-1');
-
-    // // I need to add content to the elements
-    // hourEl.text('11AM');
-    // descriptionEl.text('');
-    // saveBtnEl.text('💾');
-
-    // // I need to append the elements somewhere
-    // plannerEl.append(rowEl);
-
-    // rowEl.append(hourEl);
-    // rowEl.append(descriptionEl);
-    // rowEl.append(saveBtnEl);
-
+    displayCalendar();
 }
 
 // Event Listeners
